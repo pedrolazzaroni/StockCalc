5c4ea8ed70ce359c61af009c4b3c35eec2e7b2f3
pedrolazzaroni <phlazzaroni@gmail.com>
2025-05-08 18:09:45 -0300

    Add api

---
+++ lib/api_keys.dart
@@ -0,0 +1,1 @@
+const openAIApiKey = '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';
